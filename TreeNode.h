34ebb95594f75c00a4b71821063db93445bc3e07
Abhisingh980 <kr.abhinesh147@gmail.com>
2023-04-11 00:04:33 +0530

    ninja

---
+++ TreeNode.h
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+template <typename t>
+class TreeNode{
+    public:
+     t data;
+     vector<TreeNode<t>*> children;
+  TreeNode(t data){
+    this->data=data;
+  }
+  ~TreeNode(){
+    for(int i=0;i<children.size();i++){
+        delete children[i];
+    }
+  }
+};
