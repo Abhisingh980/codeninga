2ac62ef8028dd078825fed52e6859a84a5b8335c
Abhisingh980 <kr.abhinesh147@gmail.com>
2023-04-18 13:17:03 +0530

    modify

--- heepMinSort.cpp
+++ heepMinSort.cpp
@@ -55,7 +55,7 @@ public:
     }
  void printArray()
     {
-        for (int i = 0; i < length; ++i)
+        for (int i = length-1; i >= 0; ++i)
             cout << arr[i] << " ";
         cout << endl;
     }
